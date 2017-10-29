49bb2e176c92dd32f87fe6e88c2fa06f2d052bf5
Chirag Patel <CPatel@rightpoint.com>
2017-10-29 23:14:34 +0000

    Added manifest file to prompt user to run application as Admin.  Updated gitignore to exclude nuget packages.  Added VersionInfo.cs file to maintain application version number consistantly accross assemblies.

--- SIFLess/SIFLess.Model/Properties/AssemblyInfo.cs
+++ SIFLess/SIFLess.Model/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b63368a8-04b5-49c3-a20f-00f1fdb7205e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- SIFLess/SIFLess/Properties/AssemblyInfo.cs
+++ SIFLess/SIFLess/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8bec5909-a95d-4aed-817a-d4af72a87433")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.*")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
